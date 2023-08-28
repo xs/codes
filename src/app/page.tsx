16c6af8f5e5ef7b766570b712aabf11f3bb7ca43
Sam Balinghasay <samuel.balinghasay@gmail.com>
2023-08-28 15:25:34 -0400

    add xs.codes text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 "use client";
 
-import AspectRatioVideo from "@/components/AspectRatioVideo";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <AspectRatioVideo
-        className="rounded-lg shadow-2xl w-full overflow-hidden"
-        src="/videos/waves.mp4"
-        ratio={16 / 9}
-      />
+      <p className="text-xl font-bold items-left">
+        <div>xs.codes</div>
+      </p>
     </main>
   );
 }
