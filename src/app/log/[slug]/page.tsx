336a5564f1c4ec6790b00bdba00f044651b782e1
Sam Balinghasay <samuel.balinghasay@gmail.com>
2023-08-28 17:39:33 -0400

    notFound

--- src/app/log/[slug]/page.tsx
+++ src/app/log/[slug]/page.tsx
@@ -1,9 +1,10 @@
+import { notFound } from "next/navigation";
 import { postIndex } from "@/utils/log";
 import { Code } from "@radix-ui/themes";
 
 export default function Page({ params }: { params: { slug: string } }) {
   if (!(params.slug in postIndex)) {
-    console.log("notfound");
+    notFound();
   }
 
   return (
