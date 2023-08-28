12beaef541e5bd724b6a619fb196f448d7c110ba
Sam Balinghasay <samuel.balinghasay@gmail.com>
2023-08-28 16:22:49 -0400

    really blank

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
-import { Code, Flex, Kbd, Separator } from "@radix-ui/themes";
+import { Text, Em, Flex, Kbd, Separator } from "@radix-ui/themes";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <Flex direction="column" className="text-xl font-bold items-center">
-        <Code>xs.codes</Code>
-
-        <Separator my="3" size="4" />
-
-        <Kbd>⌘K</Kbd>
-      </Flex>
+      <Text size="3">
+        <Flex gap="3">
+          log <Separator size="2" color="amber" orientation="vertical" />
+          sketches
+        </Flex>
+      </Text>
     </main>
   );
 }
