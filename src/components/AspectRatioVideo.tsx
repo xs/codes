16bf68064274d4198fedd10aaf65b8eecff6de3f
Sam Balinghasay <samuel.balinghasay@gmail.com>
2023-08-28 14:59:44 -0400

    set page to video autoplay

---
+++ src/components/AspectRatioVideo.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import * as AspectRatio from '@radix-ui/react-aspect-ratio';
+
+interface AspectRatioVideoProps {
+  className?: string;
+  src: string;
+  ratio: number;
+}
+
+const AspectRatioVideo = ({ className, src, ratio }: AspectRatioVideoProps) => (
+  <AspectRatio.Root className={className} ratio={ratio}>
+    <video
+      autoPlay
+      muted
+      loop
+      disablePictureInPicture
+      className="object-cover w-full h-full"
+      src={src}
+    />
+  </AspectRatio.Root>
+);
+
+export default AspectRatioVideo;
