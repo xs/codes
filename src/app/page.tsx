66886300cc8dbb41159b53b48fb0476d055d76c1
Sam Balinghasay <samuel.balinghasay@gmail.com>
2023-08-28 15:51:35 -0400

    install radix ui themes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
-"use client";
+import { Code, Flex, Kbd, Separator } from "@radix-ui/themes";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <p className="text-xl font-bold items-left">
-        <div>xs.codes</div>
-      </p>
+      <Flex direction="column" className="text-xl font-bold items-center">
+        <Code>xs.codes</Code>
+
+        <Separator my="3" size="4" />
+
+        <Kbd>⌘K</Kbd>
+      </Flex>
     </main>
   );
 }
--- src/components/AspectRatioVideo.tsx
+++ src/components/AspectRatioVideo.tsx
@@ -1,23 +1,18 @@
 import React from "react";
-import * as AspectRatio from "@radix-ui/react-aspect-ratio";
 
 interface AspectRatioVideoProps {
-  className?: string;
   src: string;
-  ratio: number;
 }
 
-const AspectRatioVideo = ({ className, src, ratio }: AspectRatioVideoProps) => (
-  <AspectRatio.Root className={className} ratio={ratio}>
-    <video
-      autoPlay
-      muted
-      loop
-      disablePictureInPicture
-      className="object-cover w-full h-full"
-      src={src}
-    />
-  </AspectRatio.Root>
+const AspectRatioVideo = ({ src }: AspectRatioVideoProps) => (
+  <video
+    autoPlay
+    muted
+    loop
+    disablePictureInPicture
+    className="object-cover w-full h-full"
+    src={src}
+  />
 );
 
 export default AspectRatioVideo;
