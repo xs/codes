36299f311195588401f85dc197b94a4ed3f2d22e
Sam Balinghasay <samuel.balinghasay@gmail.com>
2023-08-29 15:51:37 -0400

    npm install syntax highlighter

--- src/app/log/[slug]/page.tsx
+++ src/app/log/[slug]/page.tsx
@@ -1,5 +1,8 @@
-import { Code } from "@radix-ui/themes";
+import { Code, Heading, Link } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
+import ReactMarkdown from "react-markdown";
+import remarkFrontmatter from "remark-frontmatter";
+import remarkGfm from "remark-gfm";
 
 import { Index, Post, fetchPostIndex } from "@/utils/log";
 
@@ -13,7 +16,21 @@ export default async function Page({ params }: { params: { slug: string } }) {
 
   const post: Post = postIndex[params.slug];
 
-  return <div dangerouslySetInnerHTML={{ __html: post.html }} />;
+  return (
+    <ReactMarkdown
+      children={post.markdown}
+      remarkPlugins={[remarkFrontmatter, remarkGfm]}
+      components={{
+        a: ({ href, title, ...props }) => <Link href={href} title={title} />,
+        h1: ({ children, ...props }) => <Heading size="6">{children}</Heading>,
+        h2: ({ children, ...props }) => <Heading size="5">{children}</Heading>,
+        h3: ({ children, ...props }) => <Heading size="4">{children}</Heading>,
+        h4: ({ children, ...props }) => <Heading size="3">{children}</Heading>,
+        h5: ({ children, ...props }) => <Heading size="2">{children}</Heading>,
+        h6: ({ children, ...props }) => <Heading size="1">{children}</Heading>,
+      }}
+    />
+  );
 }
 
 export async function generateStaticParams() {
