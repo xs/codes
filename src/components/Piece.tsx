e2c4c07266e8623b3e37e36d30d86a4b4efb5795
Sam Balinghasay <samuel.balinghasay@gmail.com>
2023-09-05 18:56:05 -0400

    add custom shader material

--- src/components/Piece.tsx
+++ src/components/Piece.tsx
@@ -3,7 +3,8 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { useRef } from "react";
-import { Mesh } from "three";
+import { Mesh, MeshPhongMaterial } from "three";
+import CustomShaderMaterial from "three-custom-shader-material";
 
 // See https://threejs.org/docs/#api/en/renderers/webgl/WebGLProgram for information
 // on attributes and uniforms available to the vertex shader.
@@ -17,12 +18,14 @@ void main() {
 }
 `;
 
+// need to declare csm_FragColor as a vec4 in the fragment shader because
+// three-custom-shader-material expects this rather than gl_FragColor
 const defaultFragmentShader = `
 uniform vec2 resolution;
 varying vec4 vPosition;
 
 void main() {
-  gl_FragColor = vec4(.05, .13, .55, 1.0);
+  csm_DiffuseColor = vec4(.05, .13, .55, 1.0);
 }
 `;
 
@@ -32,9 +35,11 @@ const CubeMesh = () => {
   return (
     <mesh ref={mesh}>
       <boxGeometry args={[4, 6, 0.001]} />
-      <shaderMaterial
+      <CustomShaderMaterial
+        baseMaterial={MeshPhongMaterial}
         vertexShader={defaultVertexShader}
         fragmentShader={defaultFragmentShader}
+        shininess={2}
         uniforms={{
           resolution: { value: [4, 6] },
         }}
@@ -46,6 +51,9 @@ const CubeMesh = () => {
 export default function Piece(): JSX.Element {
   return (
     <Canvas>
+      <ambientLight />
+      <pointLight position={[0, 0, 2]} intensity={1} />
+      <pointLight position={[0, 0, -1]} intensity={0.5} />
       <CubeMesh />
       <OrbitControls />
     </Canvas>
