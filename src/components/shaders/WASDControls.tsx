ed6119b5a35bc6b41c94072e25e2919a46d1378d
Sam Balinghasay <samuel.balinghasay@gmail.com>
2023-09-09 10:09:40 -0400

    refactor event handling

--- src/components/shaders/WASDControls.tsx
+++ src/components/shaders/WASDControls.tsx
@@ -3,72 +3,57 @@
 import { PointerLockControls } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { useControls } from "leva";
-import { useEffect, useRef } from "react";
+import { MutableRefObject, useEffect, useRef } from "react";
 import { Vector2, Vector3 } from "three";
 
 type LevaVector3 = { x: number; y: number; z: number };
 
+// TODO: allow Gallery to use this camera's position to spawn and despawn Pieces
 export default function WASDControls(): JSX.Element {
-  const sprinting = useRef<boolean>(false);
-
   function toLeva(vec: Vector3): LevaVector3 {
     return { x: vec.x, y: vec.y, z: vec.z };
   }
 
   const vec3 = { x: 0, y: 0, z: 0 };
+
   // use the leva library to display any debug info
   const [, setDebug] = useControls(() => ({
-    cameraDirection: { value: vec3 },
-    position: { value: vec3 },
+    "camera direction": { value: vec3 },
+    "current position": { value: vec3 },
     forward: 0,
     side: 0,
     "sprinting (with shift key)": false,
   }));
 
   // forward is positive, backward is negative
-  const forward = useRef(false);
-  const backward = useRef(false);
-  const right = useRef(false);
-  const left = useRef(false);
+  const forward = useRef<boolean>(false);
+  const backward = useRef<boolean>(false);
+  const right = useRef<boolean>(false);
+  const left = useRef<boolean>(false);
+  const sprinting = useRef<boolean>(false);
+
+  const keyMap: Record<string, MutableRefObject<boolean>> = {
+    Shift: sprinting,
+    w: forward,
+    W: forward,
+    a: left,
+    A: left,
+    s: backward,
+    S: backward,
+    d: right,
+    D: right,
+  };
 
   useEffect(() => {
     function onKeyDown(event: KeyboardEvent) {
-      switch (event.key) {
-        case "Shift":
-          sprinting.current = true;
-          break;
-        case "w":
-          forward.current = true;
-          break;
-        case "a":
-          left.current = true;
-          break;
-        case "s":
-          backward.current = true;
-          break;
-        case "d":
-          right.current = true;
-          break;
+      if (event.key in keyMap) {
+        keyMap[event.key].current = true;
       }
     }
 
     function onKeyUp(event: KeyboardEvent) {
-      switch (event.key) {
-        case "Shift":
-          sprinting.current = false;
-          break;
-        case "w":
-          forward.current = false;
-          break;
-        case "a":
-          left.current = false;
-          break;
-        case "s":
-          backward.current = false;
-          break;
-        case "d":
-          right.current = false;
-          break;
+      if (event.key in keyMap) {
+        keyMap[event.key].current = false;
       }
     }
 
@@ -86,28 +71,31 @@ export default function WASDControls(): JSX.Element {
     const direction = new Vector3();
     camera.getWorldDirection(direction);
 
+    // use the shift key to sprint
     const speed = sprinting.current ? 0.2 : 0.1;
 
+    // calculate forward and side movement
     const forwardMovement =
       (forward.current ? 1 : 0) - (backward.current ? 1 : 0);
     const sideMovement = (right.current ? 1 : 0) - (left.current ? 1 : 0);
 
+    // update the debug info for Leva
     setDebug({
-      cameraDirection: toLeva(direction),
-      position: toLeva(camera.position),
+      "camera direction": toLeva(direction),
+      "current position": toLeva(camera.position),
       "sprinting (with shift key)": sprinting.current,
       forward: forwardMovement,
       side: sideMovement,
     });
 
+    // "forward" is the direction the camera is looking, in the negative z axis.
+    // pointer lock controls allow the camera to look up and down, so we need to
+    // correct our movement vector to account for that.
     const horizontal = new Vector2(direction.x, direction.z).length();
     const vertical = direction.y;
-
-    // "forward" is the direction the camera is looking, which is the negative z axis
     const forwardVec = new Vector3(0, -vertical, -horizontal);
-    // "side" is the camera's right, which is the positive x axis.
 
-    // TODO: bug here where if we look to the side, then left and right are pitched? rolled? yawed?
+    // "side" is the camera's right, which is the positive x axis.
     const sideVec = new Vector3(1, 0, 0);
 
     camera.translateOnAxis(forwardVec, forwardMovement * speed);
