c13c7b4ce521d164838f81162d506969d6f0bb5f
Sam Balinghasay <samuel.balinghasay@gmail.com>
2023-10-02 18:00:15 -0400

    ortho

--- src/components/bezier/BezierCanvas.tsx
+++ src/components/bezier/BezierCanvas.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, OrthographicCamera } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Leva } from "leva";
 import { useSearchParams } from "next/navigation";
+import { DoubleSide } from "three";
 
 export default function BezierCanvas(): JSX.Element {
   const searchParams = useSearchParams();
@@ -16,20 +17,22 @@ export default function BezierCanvas(): JSX.Element {
         titleBar={{ filter: false }}
         oneLineLabels
       />
-      <Canvas
-        orthographic={true}
-        camera={{
-          position: [0, 0, 1],
-        }}
-      >
-        <mesh name="dot">
-          <circleGeometry />
-          <meshBasicMaterial color="red" />
-        </mesh>
+      <Canvas>
+        <OrthographicCamera makeDefault position={[0, 0, 10]}>
+          <mesh name="dot" position={[0, 0, 0.0001]}>
+            <circleGeometry args={[0.05]} />
+            <meshBasicMaterial color="red" />
+          </mesh>
 
-        <OrbitControls />
+          <mesh name="rect">
+            <planeGeometry args={[10, 10]} />
+            <meshBasicMaterial color="rgb(.9, .9, .9)" side={DoubleSide} />
+          </mesh>
 
-        <ambientLight />
+          <OrbitControls />
+
+          <ambientLight />
+        </OrthographicCamera>
       </Canvas>
     </>
   );
