4a0c725988683bbbbf0e2e6392549fec4d9a2786
Sam Balinghasay <samuel.balinghasay@gmail.com>
2023-10-02 17:28:39 -0400

    add bezier canvas

--- src/app/bezier/page.tsx
+++ src/app/bezier/page.tsx
@@ -1,18 +1,11 @@
 import { Box } from "@radix-ui/themes";
 
-import Gallery from "@/components/shaders/Gallery";
-import { fetchShaderIndex } from "@/utils/shaders";
+import BezierCanvas from "@/components/bezier/BezierCanvas";
 
 export default async function Page() {
-  const shaderIndex = await fetchShaderIndex();
-
-  const yvesShader = shaderIndex.byName["yves-klein"];
-  const albersShader = shaderIndex.byName["josef-albers"];
-  const neesShader = shaderIndex.byName["georg-nees"];
-
   return (
     <Box className="h-screen">
-      <Gallery shaders={[yvesShader, albersShader, neesShader]} />
+      <BezierCanvas />
     </Box>
   );
 }
