bbbae4412b867e067b2d87628bf943d0570ff7f2
Sam Balinghasay <samuel.balinghasay@gmail.com>
2023-10-02 19:12:55 -0400

    ants marching

--- src/app/bezier/page.tsx
+++ src/app/bezier/page.tsx
@@ -1,11 +1,16 @@
+"use client";
+
 import { Box } from "@radix-ui/themes";
+import { Canvas } from "@react-three/fiber";
 
 import BezierCanvas from "@/components/bezier/BezierCanvas";
 
 export default async function Page() {
   return (
     <Box className="h-screen">
-      <BezierCanvas />
+      <Canvas>
+        <BezierCanvas />
+      </Canvas>
     </Box>
   );
 }
--- src/components/bezier/BezierCanvas.tsx
+++ src/components/bezier/BezierCanvas.tsx
@@ -1,62 +1,64 @@
 "use client";
 
-import {
-  CubicBezierLine,
-  OrbitControls,
-  OrthographicCamera,
-} from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import { CubicBezierCurve, DoubleSide, Vector2 } from "three";
+import { CubicBezierLine, OrthographicCamera } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useState } from "react";
+import { DoubleSide } from "three";
 
 export default function BezierCanvas(): JSX.Element {
+  const [offset, setOffset] = useState(0);
+
+  useFrame(({ clock }) => {
+    setOffset(-clock.getElapsedTime());
+  });
+
   return (
     <>
-      <Canvas>
-        <OrthographicCamera
-          makeDefault
-          args={[-10, 10, 10, -10]}
-          // annoyingly, near and far are reversed in orthographic camera
-          zoom={40}
-          position={[0, 0, 10]}
-        >
-          <mesh name="dot-ul" position={[-5, 5, -1]}>
-            <circleGeometry args={[0.05]} />
-            <meshBasicMaterial color="blue" />
-          </mesh>
-          <mesh name="dot" position={[-4, 5, -3]}>
-            <circleGeometry args={[0.05]} />
-            <meshBasicMaterial color="magenta" />
-          </mesh>
-          <mesh name="dot" position={[3, -5, -3]}>
-            <circleGeometry args={[0.05]} />
-            <meshBasicMaterial color="red" />
-          </mesh>
-          <mesh name="dot-br" position={[5, -5, -2]}>
-            <circleGeometry args={[0.05]} />
-            <meshBasicMaterial color="green" />
-          </mesh>
-          <mesh name="rect" position={[0, 0, -10]}>
-            <planeGeometry args={[20, 20]} />
-            <meshBasicMaterial color="rgb(.9, .9, .9)" side={DoubleSide} />
-          </mesh>
+      <OrthographicCamera
+        makeDefault
+        args={[-10, 10, 10, -10]}
+        // annoyingly, near and far are reversed in orthographic camera
+        zoom={40}
+        position={[0, 0, 10]}
+      >
+        <mesh name="dot-ul" position={[-5, 5, -1]}>
+          <circleGeometry args={[0.05]} />
+          <meshBasicMaterial color="blue" />
+        </mesh>
+        <mesh name="dot" position={[-4, 5, -3]}>
+          <circleGeometry args={[0.05]} />
+          <meshBasicMaterial color="magenta" />
+        </mesh>
+        <mesh name="dot" position={[3, -5, -3]}>
+          <circleGeometry args={[0.05]} />
+          <meshBasicMaterial color="red" />
+        </mesh>
+        <mesh name="dot-br" position={[5, -5, -2]}>
+          <circleGeometry args={[0.05]} />
+          <meshBasicMaterial color="green" />
+        </mesh>
+        <mesh name="rect" position={[0, 0, -10]}>
+          <planeGeometry args={[20, 20]} />
+          <meshBasicMaterial color="rgb(.9, .9, .9)" side={DoubleSide} />
+        </mesh>
 
-          <mesh name="curve" position={[0, 0, -9]}>
-            <CubicBezierLine
-              start={[-5, 5, 0]} // Starting point
-              midA={[-4, 5, 0]} // First control point
-              midB={[3, -5, 0]} // Second control point
-              end={[5, -5, 0]} // Ending point
-              color="black" // Default
-              lineWidth={1} // In pixels (default)
-              dashed={true} // Default
-              dashSize={0.1} // In pixels (default)
-              gapSize={0.1} // In pixels (default)
-            />
-          </mesh>
+        <mesh name="curve" position={[0, 0, -9]}>
+          <CubicBezierLine
+            start={[-5, 5, 0]} // Starting point
+            midA={[-4, 5, 0]} // First control point
+            midB={[3, -5, 0]} // Second control point
+            end={[5, -5, 0]} // Ending point
+            color="black" // Default
+            lineWidth={1} // In pixels (default)
+            dashed={true} // Default
+            dashSize={0.2} // In pixels (default)
+            gapSize={0.1} // In pixels (default)
+            dashOffset={offset}
+          />
+        </mesh>
 
-          <ambientLight />
-        </OrthographicCamera>
-      </Canvas>
+        <ambientLight />
+      </OrthographicCamera>
     </>
   );
 }
