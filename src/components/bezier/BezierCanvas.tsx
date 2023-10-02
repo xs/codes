995f918858e2cab38d7ed142b0b4088aadaa62e1
Sam Balinghasay <samuel.balinghasay@gmail.com>
2023-10-02 18:47:59 -0400

    place dots correctly in ortho

--- src/components/bezier/BezierCanvas.tsx
+++ src/components/bezier/BezierCanvas.tsx
@@ -2,34 +2,35 @@
 
 import { OrbitControls, OrthographicCamera } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { Leva } from "leva";
-import { useSearchParams } from "next/navigation";
 import { DoubleSide } from "three";
 
 export default function BezierCanvas(): JSX.Element {
-  const searchParams = useSearchParams();
-  const debug = searchParams.get("debug") !== null;
   return (
     <>
-      <Leva
-        hidden={!debug}
-        hideCopyButton
-        titleBar={{ filter: false }}
-        oneLineLabels
-      />
       <Canvas>
-        <OrthographicCamera makeDefault position={[0, 0, 10]}>
-          <mesh name="dot" position={[0, 0, 0.0001]}>
+        <OrthographicCamera
+          makeDefault
+          args={[-10, 10, 10, -10]}
+          // annoyingly, near and far are reversed in orthographic camera
+          zoom={40}
+          position={[0, 0, 10]}
+        >
+          <mesh name="dot" position={[0, 0, -3]}>
             <circleGeometry args={[0.05]} />
             <meshBasicMaterial color="red" />
           </mesh>
-
-          <mesh name="rect">
+          <mesh name="dot-ul" position={[-5, 5, -1]}>
+            <circleGeometry args={[0.05]} />
+            <meshBasicMaterial color="blue" />
+          </mesh>
+          <mesh name="dot-br" position={[5, -5, -2]}>
+            <circleGeometry args={[0.05]} />
+            <meshBasicMaterial color="green" />
+          </mesh>
+          <mesh name="rect" position={[0, 0, -10]}>
             <planeGeometry args={[10, 10]} />
             <meshBasicMaterial color="rgb(.9, .9, .9)" side={DoubleSide} />
           </mesh>
-
-          <OrbitControls />
 
           <ambientLight />
         </OrthographicCamera>
