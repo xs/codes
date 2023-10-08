658f8b5191958cb9f2872f7f770b544bcde4a521
Sam Balinghasay <samuel.balinghasay@gmail.com>
2023-10-08 10:08:05 -0400

    css fixes for mobile

--- src/app/bezier/page.tsx
+++ src/app/bezier/page.tsx
@@ -2,12 +2,9 @@ import { Box } from "@radix-ui/themes";
 
 import BezierCanvas from "@/components/bezier/BezierCanvas";
 
-// TODO: refactor BezierCanvas to use Canvas from @react-three/fiber
-// so we don't need to wrap it in a Canvas component here
-// and also remove the use client directive
 export default async function Page() {
   return (
-    <Box className="h-screen">
+    <Box className="h-[calc(100dvh)]">
       <BezierCanvas />
     </Box>
   );
